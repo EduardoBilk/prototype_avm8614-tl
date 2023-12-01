6d0ee59df6894a91087dd7ef11ff53af5a0e6e6a
Eduardo Bilk <eu@eduardobilk.xyz>
2023-11-30 22:38:14 -0500

    [ADD] server

---
+++ proto02/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('update', (data) => {
+    socket.broadcast.emit('update', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    io.emit('clientDisconnected', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
